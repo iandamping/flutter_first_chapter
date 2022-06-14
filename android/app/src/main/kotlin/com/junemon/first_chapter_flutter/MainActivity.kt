a1b362db0580ce6f6e6ea2b6ba167d582cca3ed6
ian damping <ian.damping@spesolution.com>
2022-06-14 14:55:53 +0700

    first push

---
+++ android/app/src/main/kotlin/com/junemon/first_chapter_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.junemon.first_chapter_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: FirstScaffoldScreen()
+
+        // home: Scaffold(
+        //   appBar: AppBar(
+        //     title: const Text('Hello, world!'),
+        //   ),
+        //   body: const Center(
+        //     child: BiggerText(text: 'Hello, world!'),
+        //     // child: Heading(text: 'Hello, world!'),
+        //   ),
+        // ),
+        );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  const Heading({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold));
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+
+  const BiggerText({required this.text});
+
+  @override
+  State createState() {
+    return _BiggerTextState();
+  }
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _textSize = 32.0;
+              });
+            },
+            child: const Text("Perbesar"))
+      ],
+    );
+  }
+}
+
+class FirstScaffoldScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+            icon: const Icon(Icons.menu, color: Colors.white),
+            onPressed: () {}),
+        title: const Text("First Chapter"),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.search, color: Colors.white))
+        ],
+      ),
+      body: Center(
+        child: Text("Aku adalah body"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () {},
+      ),
+    );
+  }
+}
